db1ae44af0aa0a6066f4620dc90de463943d395f
projectASP <37558167+projectASP@users.noreply.github.com>
2018-04-20 20:10:36 -0500

    Can't upload all files, uploaded files generally altered
    -sheilf

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+// ~step 1: Make Empty ASP file -> checkbox MVC folders
+// ~step 2: Create empty Home Controller
+// ~step 3: alter actionresult index() to return a string 
+
+//===================
+
+// ~step 8: Add a View to be returned. Right click index method and add view. Just hit add, don't alter params.
+// ~Step 9: take a look at the params in html:  shared _Layout and Home View
+// ~Step 10: If your ASP installed bootstrap when you created the file. 
+            //Replace the html from !Doctype onward with the content from page 28 in the tutorial
+            //CHANGE string Index() to ActionResult and return View() 
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            //return "String returned from HomeController.Index()";
+            return View();
+        }
+    }
+}
---
+++ Controllers/StoreController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+// ~step 4: Create empty StoreController
+// ~step 5:  create string Index, string Browse and string Details
+// ~step 6: Run project (CTRL + F5 on windows run without debug) and examine these routes: 
+            //localhost:yourport/Store
+            //localhost:yourport/Store/Browse
+            //localhost:yourport/Store/Details
+
+// ~step 7: add some dynamic values to the routes.
+            //check out Browse() again and the new functions.
+
+// ~step 8: Do a similar message passing procedure process to Details() with an id number instead.
+
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: /Store
+        public string Index()
+        {
+            return "String returned from StoreController.Index()";
+        }
+
+        //GET: /Store/Browse?genre=Disco
+        public string Browse(string genre)
+        { 
+            //HttpUtility does this: /Store/Browse?Genre=<script>window.location=’http://hackersite.com’</script>.
+
+            string message = HttpUtility.HtmlEncode("StoreController.Browse(string genre) returns Genre = " + genre);
+            return message;
+
+        }
+
+        //GET: /Store/Details/id
+        public string Details(int id)
+        {
+            //test localhost:portnum/Store/Details/anyInteger
+            string message = "StoreController.Details(int id) returns ID = " + id;
+            return message;
+        }
+    }
+}
